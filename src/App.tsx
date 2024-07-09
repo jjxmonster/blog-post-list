5229f98d49a3e459e51f5c4eea725658b77790ca
jjxmonster <kuba.tarabasz@wp.pl>
2024-07-10 00:56:44 +0200

    [Blog List] Create container for search logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,9 @@
-import AuthorSelect from "@/components/AuthorSelect";
-import { useBlogPosts } from "@/hooks/useBlogPostsData";
-import { filterPosts } from "@/lib/utils";
-import { useState } from "react";
-import { BlogPostsListContainer } from "./containers/BlogPostsListContainer";
+import { BlogPostSearchContainer } from "./containers/BlogPostSearchContainer";
 
 function App() {
-	const [author, setAuthor] = useState("");
-	const { data: posts } = useBlogPosts();
-
-	const filteredPosts = filterPosts({
-		posts: posts || [],
-		key: "userId",
-		query: author,
-	});
-
 	return (
 		<main className="mx-auto max-w-5xl px-6 pt-10 md:px-0">
-			<div className="pt-24" />
-			<h1 className="text-2xl text-black text-center">Blog Posts</h1>
-			<div className="pt-8" />
-			<form className="space-x-4 flex items-end justify-center">
-				<AuthorSelect author={author} setAuthor={setAuthor} />
-			</form>
-			<div className="pt-12" />
-			<BlogPostsListContainer posts={filteredPosts} />
-			<div className="pt-16" />
+			<BlogPostSearchContainer />
 		</main>
 	);
 }
---
+++ src/containers/BlogPostSearchContainer.tsx
@@ -0,0 +1,31 @@
+import AuthorSelect from "@/components/AuthorSelect";
+import { useBlogPosts } from "@/hooks/useBlogPostsData";
+import { filterPosts } from "@/lib/utils";
+import { useState } from "react";
+import { BlogPostsListContainer } from "./BlogPostsListContainer";
+
+export const BlogPostSearchContainer = () => {
+	const [author, setAuthor] = useState("");
+	const { data: posts } = useBlogPosts();
+
+	const filteredPosts = filterPosts({
+		posts: posts || [],
+		key: "userId",
+		query: author,
+	});
+	return (
+		<>
+			<div className="pt-24" />
+			<h1 className="text-4xl text-black font-medium text-center">
+				Blog Posts
+			</h1>
+			<div className="pt-8" />
+			<form className="space-x-4 flex items-end justify-center">
+				<AuthorSelect author={author} setAuthor={setAuthor} />
+			</form>
+			<div className="pt-12" />
+			<BlogPostsListContainer posts={filteredPosts} />
+			<div className="pt-16" />
+		</>
+	);
+};
