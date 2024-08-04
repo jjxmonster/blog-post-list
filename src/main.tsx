4c445d61d813a2afaf3883309834e9d06126498b
jjxmonster <kuba.tarabasz@wp.pl>
2024-08-04 23:47:04 +0200

    [Blog List] Feature toggling url correct

--- src/main.tsx
+++ src/main.tsx
@@ -17,7 +17,7 @@ const queryClient = new QueryClient({
 const envName = import.meta.env.MODE || "preview";
 
 const featurevisor = createInstance({
-	datafileUrl: `https://d9f0wbtdbs2sh.cloudfront.net/${envName}/datafile-tag-all.json`,
+	datafileUrl: `https://d9f0wbtdbs2sh.cloudfront.net/datafiles/${envName}/datafile-tag-all.json`,
 });
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
